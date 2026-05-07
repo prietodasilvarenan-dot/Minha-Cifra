c7a3d6956c1452196305db4faed73511b211cd83
prietodasilvarenan-dot <prietodasilvarenan@gmail.com>
2026-05-07 09:33:27 -0300

    criaçao da classe User

---
+++ backend/model/classes/user.ts
@@ -0,0 +1,9 @@
+class User {
+    public email: string;
+    private password: string;
+    
+    constructor(email: string, password: string){
+        this.email = email
+        this.password = password
+    }
+}
