7f2ec406d832db890b022f37acda2d948809a839
prietodasilvarenan-dot <prietodasilvarenan@gmail.com>
2026-08-10 10:28:43 -0300

    refactor(_layout): changes on navbar router & styles

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,75 +4,102 @@ import { useColorScheme } from "@/src/hooks/use-color-scheme";
 import { Feather } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 import React from "react";
+import { View } from "react-native";
 
 export default function TabLayout() {
-	const colorScheme = useColorScheme();
+    const colorScheme = useColorScheme();
 
-	return (
-		<Tabs
-			screenOptions={{
-				headerShown: false,
-				tabBarButton: HapticTab,
-				tabBarActiveTintColor: "#006BFF",
-				tabBarInactiveTintColor: "#888",
-				tabBarStyle: {
-					backgroundColor: Colors[colorScheme ?? "light"].background,
-					height: 65,
-					paddingBottom: 8,
-					paddingTop: 8,
-					borderTopWidth: 0,
-					elevation: 12,
-					shadowColor: "#000",
-					shadowOpacity: 0.08,
-					shadowRadius: 10,
-				},
-			}}
-		>
-			<Tabs.Screen
-				name="index"
-				options={{
-					title: "Início",
-					tabBarIcon: ({ color }) => (
-						<Feather name="home" size={24} color={color} />
-					),
-				}}
-			/>
-			<Tabs.Screen
-				name="barGraph"
-				options={{
-					title: "Estatísticas",
-					tabBarIcon: ({ color }) => (
-						<Feather name="bar-chart-2" size={24} color={color} />
-					),
-				}}
-			/>
-			<Tabs.Screen
-				name="calculator"
-				options={{
-					title: "Calculadora",
-					tabBarIcon: ({ color }) => (
-						<Feather name="percent" size={24} color={color} />
-					),
-				}}
-			/>
-			<Tabs.Screen
-				name="pizzaGraph"
-				options={{
-					title: "Gastos",
-					tabBarIcon: ({ color }) => (
-						<Feather name="trending-down" size={24} color={color} />
-					),
-				}}
-			/>
-			<Tabs.Screen
-				name="perfil"
-				options={{
-					title: "Perfil",
-					tabBarIcon: ({ color }) => (
-						<Feather name="user" size={24} color={color} />
-					),
-				}}
-			/>
-		</Tabs>
-	);
+    return (
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+                tabBarButton: HapticTab,
+                tabBarActiveTintColor: "#006BFF",
+                tabBarInactiveTintColor: "#888",
+                tabBarStyle: {
+                    backgroundColor: Colors[colorScheme ?? "light"].background,
+                    height: 65,
+                    paddingBottom: 4,
+                    paddingTop: 12,
+                    borderTopWidth: 0,
+                    elevation: 12,
+                    shadowColor: "#000",
+                    shadowOpacity: 0.08,
+                    shadowRadius: 10,
+                },
+            }}
+        >
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: "",
+                    tabBarIcon: ({ color }) => (
+                        <Feather name="home" size={24} color={color} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="graph"
+                options={{
+                    title: "",
+                    tabBarIcon: ({ color }) => (
+                        <Feather name="bar-chart-2" size={24} color={color} />
+                    ),
+                }}
+            />
+            
+            <Tabs.Screen
+                name="newRelease"
+                options={{
+                    title: "",
+                    tabBarLabel: () => null,
+                    tabBarIcon: () => (
+                        <View
+                            style={{
+                                width: 67,
+                                height: 48,
+                                borderRadius: 24,
+                                backgroundColor: "#006BFF",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                shadowOffset: { width: 0, height: 4 },
+                                shadowOpacity: 0.3,
+                                shadowRadius: 5,
+                            }}
+                        >
+                            <Feather name="plus" size={28} color="#FFFFFF" />
+                        </View>
+                    ),
+                }}
+            />
+
+            <Tabs.Screen
+                name="calculator"
+                options={{
+                    title: "",
+                    tabBarIcon: ({ color }) => (
+                        <Feather name="percent" size={24} color={color} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="pizzaGraph"
+                options={{
+                    title: "",
+                    tabBarIcon: ({ color }) => (
+                        <Feather name="trending-down" size={24} color={color} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="perfil"
+                options={{
+                    title: "",
+                    tabBarIcon: ({ color }) => (
+                        <Feather name="user" size={24} color={color} />
+                    ),
+                }}
+            />
+        </Tabs>
+    );
 }
