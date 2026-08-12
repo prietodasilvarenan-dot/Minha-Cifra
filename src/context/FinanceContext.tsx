8709f7037ae2e7309f8336a0a9a49ff9def2c17e
prietodasilvarenan <prietodasilvarenan@gmail.com>
2026-08-12 18:19:48 -0300

    feat: add investments wallet

--- src/context/FinanceContext.tsx
+++ src/context/FinanceContext.tsx
@@ -1,8 +1,13 @@
-import React, { createContext, useContext, useState, ReactNode } from "react";
+import React, {
+  createContext,
+  ReactNode,
+  useContext,
+  useState,
+} from "react";
 
 export interface ItemFinance {
   id: string;
-  title: string;
+  title: string | null;
   value: number;
   tag: string;
 }
@@ -10,14 +15,29 @@ export interface ItemFinance {
 interface FinanceContextData {
   itemsEarn: ItemFinance[];
   setItemsEarn: React.Dispatch<React.SetStateAction<ItemFinance[]>>;
+
+  itemsInvestments: ItemFinance[];
+  setItemsInvestments: React.Dispatch<React.SetStateAction<ItemFinance[]>>;
+
   itemsLost: ItemFinance[];
   setItemsLost: React.Dispatch<React.SetStateAction<ItemFinance[]>>;
+
   totalEarn: number;
+  totalInvestments: number;
   totalLost: number;
+
   balance: number;
+
   tagsEarn: string[];
+  tagsInvestments: string[];
   tagsLost: string[];
+
+  addTagEarn: (tag: string) => void;
+  addTagInvestments: (tag: string) => void;
+  addTagLost: (tag: string) => void;
+
   addEarn: (item: Omit<ItemFinance, "id">) => void;
+  addInvestments: (item: Omit<ItemFinance, "id">) => void;
   addLost: (item: Omit<ItemFinance, "id">) => void;
 }
 
@@ -25,47 +45,137 @@ const FinanceContext = createContext<FinanceContextData>(
   {} as FinanceContextData,
 );
 
-export function FinanceProvider({ children }: { children: ReactNode }) {
+export function FinanceProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
   const [itemsEarn, setItemsEarn] = useState<ItemFinance[]>([]);
-
+  const [itemsInvestments, setItemsInvestments] =
+    useState<ItemFinance[]>([]);
   const [itemsLost, setItemsLost] = useState<ItemFinance[]>([]);
 
-  const tagsEarn = Array.from(new Set(itemsEarn.map((item) => item.tag)));
-  const tagsLost = Array.from(new Set(itemsLost.map((item) => item.tag)));
+  const [tagsEarn, setTagsEarn] = useState<string[]>([]);
+  const [tagsInvestments, setTagsInvestments] = useState<string[]>([]);
+  const [tagsLost, setTagsLost] = useState<string[]>([]);
+
+  const addTagEarn = (tag: string) => {
+    const tagFormatada = tag.trim();
+
+    if (!tagFormatada) return;
+
+    setTagsEarn((prev) => {
+      if (prev.includes(tagFormatada)) {
+        return prev;
+      }
+
+      return [...prev, tagFormatada];
+    });
+  };
+
+  const addTagInvestments = (tag: string) => {
+    const tagFormatada = tag.trim();
+
+    if (!tagFormatada) return;
+
+    setTagsInvestments((prev) => {
+      if (prev.includes(tagFormatada)) {
+        return prev;
+      }
+
+      return [...prev, tagFormatada];
+    });
+  };
+
+  const addTagLost = (tag: string) => {
+    const tagFormatada = tag.trim();
+
+    if (!tagFormatada) return;
+
+    setTagsLost((prev) => {
+      if (prev.includes(tagFormatada)) {
+        return prev;
+      }
+
+      return [...prev, tagFormatada];
+    });
+  };
 
   const addEarn = (newItem: Omit<ItemFinance, "id">) => {
     const itemWithId: ItemFinance = {
       ...newItem,
       id: Date.now().toString(),
     };
+
     setItemsEarn((prev) => [...prev, itemWithId]);
   };
+
+  const addInvestments = (newItem: Omit<ItemFinance, "id">) => {
+    const itemWithId: ItemFinance = {
+      ...newItem,
+      id: Date.now().toString(),
+    };
+
+    setItemsInvestments((prev) => [...prev, itemWithId]);
+  };
+
 
   const addLost = (newItem: Omit<ItemFinance, "id">) => {
     const itemWithId: ItemFinance = {
       ...newItem,
       id: Date.now().toString(),
     };
+
     setItemsLost((prev) => [...prev, itemWithId]);
   };
 
-  const totalEarn = itemsEarn.reduce((acc, item) => acc + item.value, 0);
-  const totalLost = itemsLost.reduce((acc, item) => acc + item.value, 0);
-  const balance = totalEarn - totalLost;
+
+  const totalEarn = itemsEarn.reduce(
+    (acc, item) => acc + item.value,
+    0,
+  );
+
+  const totalInvestments = itemsInvestments.reduce(
+    (acc, item) => acc + item.value,
+    0,
+  );
+
+  const totalLost = itemsLost.reduce(
+    (acc, item) => acc + item.value,
+    0,
+  );
+
+  const balance =
+    totalEarn - (totalLost + totalInvestments);
 
   return (
     <FinanceContext.Provider
       value={{
         itemsEarn,
         setItemsEarn,
+
+        itemsInvestments,
+        setItemsInvestments,
+
         itemsLost,
         setItemsLost,
+
         totalEarn,
+        totalInvestments,
         totalLost,
+
         balance,
+
         tagsEarn,
+        tagsInvestments,
         tagsLost,
+
+        addTagEarn,
+        addTagInvestments,
+        addTagLost,
+
         addEarn,
+        addInvestments,
         addLost,
       }}
     >
