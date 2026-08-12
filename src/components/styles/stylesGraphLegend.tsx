b36b27d8ed8fa54f1d0ead1f43ea1c1f29c051c8
prietodasilvarenan-dot <prietodasilvarenan@gmail.com>
2026-08-12 09:28:32 -0300

    fix(styles): cor da legenda do grafico em barras

---
+++ src/components/styles/stylesGraphLegend.tsx
@@ -0,0 +1,33 @@
+import { StyleSheet } from "react-native";
+
+export const getGraphLegendStyle = (isDark: boolean) => 
+    StyleSheet.create({
+        legendContainer: {
+            flexDirection: "row",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+            width: "100%",
+            marginTop: 15,
+            marginBottom: 10,
+        },
+
+        legendItem: {
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 7,
+        },
+
+        legendBox: {
+            width: 15,
+            height: 15,
+            borderRadius: 4,
+        },
+
+        legendText: {
+            fontSize: 13,
+            color: isDark
+            ? "#AEBBD0"
+            : "#070808",
+            fontWeight: "600",
+        },
+});
